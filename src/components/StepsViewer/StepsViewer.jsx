d6a3a9a7d2569412f292923fa841be8c75ad519b
Konstantin <k.kokhanenko@yandex.ru>
2024-03-24 20:39:24 +0300

    Update StepsViewer.jsx

--- src/components/StepsViewer/StepsViewer.jsx
+++ src/components/StepsViewer/StepsViewer.jsx
@@ -1,15 +1,25 @@
-export const StepsViewer = (props) => {
-    const { items } = props;
+export const StepsViewer = (props) => {   
+    const { items, handle } = props;
+    const sortedItems = items.sort((a, b) => a.date > b.date ? 1 : -1);
 
-    return (
-        <>
-            <div className="box">
-                {
-                    Object.keys(items).map(key => {
-                        <div key={key}>{items[key]}</div> 
-                    })
-                }
+    const data = sortedItems.map(function(item) {
+        return <div className="columns" key={item.date}>
+            <div className="column is-one-third">{item.date}</div>
+            <div className="column is-one-third">{item.distance}</div>
+            <div className="column is-one-third">
+                <button className="button is-primary" data-date={item.date} onClick={handle}>Удалить</button>
             </div>
-        </>
-    )
+        </div>;
+     });
+   
+    return (
+        <div className="box">
+            <div className="columns">
+                <div className="column is-one-third"><b>Дата</b></div>
+                <div className="column is-one-third"><b>Дистанция</b></div>
+                <div className="column is-one-third"><b>Дейтсвия</b></div>
+            </div>
+            {data}
+        </div>
+    )   
 }
