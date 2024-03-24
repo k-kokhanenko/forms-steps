790278598ec11ed0cd5ee27dbce4c1f6d8f1b346
Konstantin <k.kokhanenko@yandex.ru>
2024-03-24 20:39:05 +0300

    Update Steps.jsx

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -6,21 +6,39 @@ import { StepsViewer } from '../StepsViewer/StepsViewer';
 
 
 export const Steps = () => {
-    const [items, setItems] = useState({});
+    const [items, setItems] = useState([]);
 
-    const handle = (date, distance) => {
-        let currentItems = items;
-        currentItems[date] === undefined ? 
-            currentItems[date] = Number(distance) : 
-            currentItems[date] += Number(distance);
+    const handleUpdate = (date, distance) => {
+        let newItems = Object.assign([], items);
+
+        let update = false;
+        newItems.map(function(item) {
+            if (item.date == date) {
+                update = true;
+                item.distance = Number(item.distance) + Number(distance);
+            }
+        });
+
+        if (!update) {
+            newItems.push({
+                date : date,
+                distance : distance
+            })
+        }
         
-        setItems(...currentItems); 
+        setItems(newItems);        
+    }
+
+    const handleDelete = (event) => {
+        const date = event.target.dataset.date;
+        let newItems = items.filter(item => item.date !== date);
+        setItems(newItems);                
     }
 
     return (
-    <>
-        <StepForm handle={handle}/>
-        <StepsViewer items={items}/>
-    </>
-  )
+        <>
+            <StepForm handle={handleUpdate}/>
+            <StepsViewer items={items} handle={handleDelete}/>
+        </>
+    )
 }
